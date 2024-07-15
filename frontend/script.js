4fd2c77ab981ac22a3ebec2d739f77e19d4f94b8
Gregory Zelikman <gregory.zelikman@protonmail.com>
2024-07-15 15:05:28 +0200

    very epic

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,14 @@
 $(function(){
-    document.getElementById('content').innerHTML = "Loading...";
+    document.getElementById('drinks').innerHTML = "Loading...";
     $.ajax({
         url: 'http://127.0.0.1:7999/',
         type: 'get',
     }).done(function (response) {
         console.log("Api call worked", response);
-        document.getElementById('content').innerHTML = response;
+        console.log(response["drinks"].length);
+        for (var i = 0; i < response["drinks"].length; i++) {
+            document.getElementById("drinks").innerHTML = response["drinks"][i]["name"] + " - " + response["drinks"][i]["price"];
+        }
     }).fail(function (error) {
         console.log(error);
     });
