8047596b4224aa59d1d4806175a87e4680aabc3e
greg <gregory.zelikman@protonmail.com>
2024-08-09 17:02:06 +0200

    Some changes

---
+++ adminPanel/script.js
@@ -0,0 +1,23 @@
+$(setInterval(function(){
+    $.ajax({
+        url: 'http://127.0.0.1:7999/getOrders',
+        type: 'get',
+    }).done(function (response) {
+        aktuelleTische = "";
+        for(var i = 0; i < response["Tische"].length; i++){
+            var number = "";
+            for (j in response["Tische"][i]){
+                number = j;
+            }
+            if(response["Tische"][i][number].length > 0 ){
+                aktuelleTische += "<div id=\"tisch\"> Tisch Nr. " + number + " has " + response["Tische"][i][number].length + " Orders</div>";
+            }
+            else{
+                aktuelleTische += "<div id=\"tisch\"> Tisch Nr. " + number + " has 1 Order</div>";
+            }
+        }
+        document.getElementById("Tische").innerHTML = aktuelleTische;
+    }).fail(function (error) {
+        console.log(error);
+    });
+}, 2000));
